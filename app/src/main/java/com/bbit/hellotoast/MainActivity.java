36781f1e27fe59ff055305e241c2a77cd7f0e8ca
Psycadelik <francisadrian9@hotmail.com>
2020-01-24 20:00:18 +0300

    update: add toast logic

--- app/src/main/java/com/bbit/hellotoast/MainActivity.java
+++ app/src/main/java/com/bbit/hellotoast/MainActivity.java
@@ -3,6 +3,8 @@ package com.bbit.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void countUp(View view) {
+    }
+
+    public void showToast(View view) {
+        //create a toast to view it
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+
     }
 }
